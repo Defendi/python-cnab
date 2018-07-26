5b935687a58bf8a901e62651eff7e76d24c97d85
defendi <alexandre_defendi@hotmail.com>
2018-07-26 17:45:44 -0300

    ajuste do nosso número do seguimento P da CEF

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-cnab',
-    version='0.1.21',
+    version='0.1.22',
     author='Trustcode',
     author_email='suporte@trustcode.com.br',
     url='https://github.com/Trust-Code/python-cnab',
